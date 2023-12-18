4f4a75b112e836e87abb29e9c299745c1e4814ca
amit <solomonya@gmail.com>
2023-12-18 04:25:22 +0200

    extract utils functionality

---
+++ src/utils/file_handler.go
@@ -0,0 +1,42 @@
+package filehandler
+
+import (
+	"os"
+	"strings"
+)
+
+func ReadURLsFromFile(filename string) ([]string, error) {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(content) == 0 {
+		return []string{}, nil // Return an empty string array if content is empty
+	}
+
+	return strings.Split(string(content), "\n"), nil
+}
+
+func GetURLsFilePath(relativeURLsFile, baseURLsFile string) string {
+	if _, err := os.Stat(relativeURLsFile); err == nil {
+		return relativeURLsFile // Use relative path if file exists
+	}
+
+	if _, err := os.Stat(baseURLsFile); err == nil {
+		return baseURLsFile // Use absolute path if file exists
+	}
+
+	return "" // If file not found in both relative and absolute paths
+}
+
+func CreateOutputDirectory(outputDirectory string) error {
+	if _, err := os.Stat(outputDirectory); os.IsNotExist(err) {
+		return os.Mkdir(outputDirectory, 0755)
+	} else if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Other file handling functions...
